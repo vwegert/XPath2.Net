b5b44da5b91309dc7a7b5e413629cc5ed6440699
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-05-13 17:06:56 +0200

    Fix #2

--- src/XPath2.Extensions/Properties/AssemblyInfo.cs
+++ src/XPath2.Extensions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XPath2.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Stef Heyenrath 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
