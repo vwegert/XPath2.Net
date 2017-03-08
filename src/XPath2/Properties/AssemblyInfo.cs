010d7a20cf30f4c4dc5fe6f3206876fc329b4059
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-03-08 12:06:24 +0100

    Initial vs2017 support

--- src/XPath2/Properties/AssemblyInfo.cs
+++ src/XPath2/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
-﻿using System.Reflection;
+﻿//using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XPath2")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XPath2")]
-[assembly: AssemblyCopyright("Copyright © Semyon A. Chertkov, Stef Heyenrath 2011-2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("XPath2")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("XPath2")]
+//[assembly: AssemblyCopyright("Copyright © Semyon A. Chertkov, Stef Heyenrath 2011-2016")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
